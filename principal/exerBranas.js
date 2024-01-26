5e5a40647f16e35fc4b6486418d6b4ff4b37e5ed
Marcos Miller <marcos_miller@M.Miller>
2024-01-26 15:53:45 -0300

    finishCommitTheDayy

--- principal/exerBranas.js
+++ principal/exerBranas.js
@@ -6,26 +6,51 @@
 //Extraia as colunas da tabela e armazene em uma variável chamada "columns".
 //Manipule a variável "columns", separando cada coluna com seu respectivo tipo, em uma string separada.
 
-/* const statement = "create table author (id number, name string, age number, city string, state string, country string)";
-const regexp = /create table ([a-z]+) \((.+)\)/;
-const parsedStatement = statement.match(regexp);
-const tableName = parsedStatement[1];
-let columns = parsedStatement[2];
-columns = columns.split(", ");
-console.log(tableName);
-console.log(columns); */
+const DataBaseError = (comando, message) => {
+    this.comando = comando;
+    this.message = message;
+};
+
+const database = {
+    tables: {},
+    comandoAnalisado: {},
+    createTable(comando) {
+        const regExp = /create table ([a-z]+) \((.+)\)/;
+        this.comandoAnalisado = comando.match(regExp);
+        const tableName = this.comandoAnalisado[1];
+        this.tables[tableName] = {
+            colunas: {},
+            data: []
+        };
+        const columns = this.comandoAnalisado[2].split(", ");
+        for (let column of columns) {
+            column = column.trim().split(" ");
+            const name = column[0];
+            const type = column[1];
+            this.tables[tableName].colunas[name] = type;
+        }
+        },
+        execute(comando){
+            if(comando.startsWith("create table")) {
+                return this.craateTable(comando);
+            }
+        const message = `Syntax erro "${comando}"`;
+        throw new DataBaseError(comando, message);
+    }
+};
 
 const comando = "create table author (id number, name string, age number, city string, state string, country string)";
+database.createTable(comando);
 
-let regExp = /create table ([a-z]+) \((.+)\)/;
-let comandoAnalisado = comando.match(regExp);
-let colunas = comandoAnalisado[2];
-colunas = colunas.split(", ");
-
-console.log("Comando em Análise: " + comandoAnalisado[0]);
-console.log("************");
-console.log("Nome do autor: " + comandoAnalisado[1]);
-console.log("************");
-console.log("Colunas do comando: " + comandoAnalisado[2]);
-console.log("************");
-console.log(colunas);
+try {
+    //database.execute("select id, name from author");
+    console.log("Comando em Análise: " + JSON.stringify(database.comandoAnalisado));
+    console.log("************");
+    console.log("Nome da tabela: " + database.comandoAnalisado[1]);
+    console.log("************");
+    console.log("Colunas do comando: " + database.comandoAnalisado[2]);
+    console.log("************");
+    console.log("Colunas do objeto database: " + JSON.stringify(database.tables.author.colunas, undefined, 2));
+} catch (e) {
+    console.log(e.message);
+}
